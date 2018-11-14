cd5dd2697d0d49527431d4784bd31a60e5181eac
Greer <gviau2@gmail.com>
2018-11-14 11:58:46 -0500

    Don't need string dependency

--- BeeMovieLSTM.py
+++ BeeMovieLSTM.py
@@ -1,4 +1,4 @@
-import os, pickle, tflearn, urllib, string
+import os, pickle, tflearn, urllib
 from tflearn.data_utils import *
 
 url = "https://gist.githubusercontent.com/The5heepDev/a15539b297a7862af4f12ce07fee6bb7/raw/7164813a9b8d0a3b2dcffd5b80005f1967887475/entire_bee_movie_script"
