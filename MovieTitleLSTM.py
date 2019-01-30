2691978a1f3b5059cffca5682630aa30169736b3
greerviau <gviau2@gmail.com>
2019-01-30 00:07:59 -0500

    Adjusted for movie titles

--- MovieTitleLSTM.py
+++ MovieTitleLSTM.py
@@ -1,19 +1,14 @@
 import os, pickle, tflearn, urllib
 from tflearn.data_utils import *
 
-url = "https://gist.githubusercontent.com/The5heepDev/a15539b297a7862af4f12ce07fee6bb7/raw/7164813a9b8d0a3b2dcffd5b80005f1967887475/entire_bee_movie_script"
-path = "bee_movie_script.txt"
+path = "movie_titles.txt"
 char_idx_file = 'char_idx.pickle'
 
 #set some variables
 maxlen = 25
 learning_rate = 0.001
-max_epochs = 50
+max_epochs = 30
 batch_size = 128
-
-#if file doesnt exist, get it from url
-if not os.path.isfile(path):
-    urllib.request.urlretrieve(url, path)
 
 #load pickle
 char_idx = None
@@ -39,19 +34,22 @@ g = tflearn.fully_connected(g, len(char_idx), activation='softmax')
 g = tflearn.regression(g, optimizer='adam', loss='categorical_crossentropy', learning_rate=learning_rate)
 
 #generate sequence
-m = tflearn.SequenceGenerator(g, dictionary=char_idx, seq_maxlen=maxlen, clip_gradients=5.0, checkpoint_path='model_bee_movie')
+m = tflearn.SequenceGenerator(g, dictionary=char_idx, seq_maxlen=maxlen, clip_gradients=5.0, checkpoint_path='model_movie_titles')
 
 #training
 for i in range(max_epochs):
     seed = random_sequence_from_textfile(path, maxlen)
-    m.fit(X, Y, validation_set=0.1, batch_size=batch_size, snapshot_epoch=False, show_metric=True, n_epoch=1, run_id='bee_movie')
+    m.fit(X, Y, validation_set=0.1, batch_size=batch_size, snapshot_epoch=False, show_metric=True, n_epoch=1, run_id='movie_titles')
     print("-- Testing --")
-    print("-- Testing with temp of 1.0 --")
-    print(m.generate(maxlen*2, temperature=1.0, seq_seed=seed))
-    print("-- Testing with temp of 0.5 --")
-    print(m.generate(maxlen*2, temperature=0.5, seq_seed=seed))
+    print("-- Temp of 1.0 --")
+    print(m.generate(100, temperature=1.0, seq_seed=seed))
+    print("-- Temp of 0.5 --")
+    print(m.generate(100, temperature=0.5, seq_seed=seed))
 
 #final output
-print("-- Final Script --")
 seed = random_sequence_from_textfile(path, maxlen)
-print(m.generate(500, temperature=1.0, seq_seed=seed))
+print("-- Final Movie Titles --")
+print("-- Temp of 1.0 --")
+print(m.generate(100, temperature=1.0, seq_seed=seed))
+print("-- Temp of 0.5 --")
+print(m.generate(100, temperature=0.5, seq_seed=seed))
